adba844624323ad2f680c9e9a8efd4a6ef3bb323
Samit-K <samit.k@technovert.com>
2025-01-10 17:22:53 +0530

    Made changes for SauceDemo

--- features/steps/cart.py
+++ features/steps/cart.py
@@ -1,9 +1,4 @@
 from behave import *
-from selenium.common import NoSuchElementException
-
-from pages.CartPage import CartPage
-from pages.HomePage import HomePage
-from pages.ProductPage import ProductPage
 
 
 @then(u'I see the "first two" products added to the cart')
